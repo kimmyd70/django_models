ccaa7a7575c2ca31e03d23859a681afb8d5f505c
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-03-05 20:39:56 -0800

    templates updated

--- snacks/models.py
+++ snacks/models.py
@@ -5,3 +5,6 @@ class Snack(models.Model):
     name = models.CharField(max_length=64, default = '')
     purchaser = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
     description = models.TextField(default = '')
+
+    def __str__(self):
+        return self.name
